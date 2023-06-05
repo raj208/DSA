f1d7a62824e8a360783307a9b777a1d0cd2f26aa
RAJENDRA <rajendrajfc1021@gmail.com>
2023-06-06 01:43:14 +0530

    stack

---
+++ STL/stack.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main(){
+    stack<string> s;
+    s.push("messi");
+    s.push("neymar");
+    s.push("suarez");
+
+    cout<<"Top Element-> "<<s.top()<<endl;
+
+    s.pop();
+    cout<<"Top Element-> "<<s.top()<<endl;
+
+    cout<<"Size "<<s.size()<<endl;
+
+    cout<<"Empty or not "<<s.empty()<<endl;
+    return 0;
+}
