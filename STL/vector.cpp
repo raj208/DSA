1c02800716c6fd863d6c25467c7d5246c3a07e34
RAJENDRA <rajendrajfc1021@gmail.com>
2023-06-19 01:20:10 +0530

    operating 2D matrix using vector

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -57,3 +57,34 @@ int main(){
     
     return 0;
 }
+
+
+// ************operating 2D matrix*************
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+
+// int main() {
+//     int rows = 3;
+//     int columns = 3;
+
+//     // Create a matrix using a vector of vectors
+//     vector<vector<int>> matrix(rows, vector<int>(columns));
+
+//     // Assign values to the matrix
+//     for (int i = 0; i < rows; i++) {
+//         for (int j = 0; j < columns; j++) {
+//             cin>>matrix[i][j];
+//         }
+//     }
+
+//     // Print the matrix
+//     for (int i = 0; i < rows; i++) {
+//         for (int j = 0; j < columns; j++) {
+//             cout << matrix[i][j] << " ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
