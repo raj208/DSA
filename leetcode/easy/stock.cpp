80188d6f4cd928cb96b683cc6f34d38f892f3989
RAJENDRA <rajendrajfc1021@gmail.com>
2023-06-23 17:07:31 +0530

    stock

--- leetcode/easy/stock.cpp
+++ leetcode/easy/stock.cpp
@@ -31,3 +31,34 @@ int main(){
     cout<<"Maximum profit is:"<<Maxprofit<<endl;;
     return 0;
 }
+
+
+/*
+
+***********DIFFERENT METHOD****************
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.empty())
+            return 0;
+
+        int maxProfit = 0;
+        int minPrice = prices[0];
+
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else {
+                int currentProfit = prices[i] - minPrice;
+                if (currentProfit > maxProfit) {
+                    maxProfit = currentProfit;
+                }
+            }
+        }
+
+        return maxProfit;
+    }
+};
+
+*/
